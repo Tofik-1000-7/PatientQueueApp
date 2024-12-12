4b1a150a28d3580b3c11f6537b2d69dc359554d7
Venikov E.A <ven741@yandex.ru>
2024-12-12 17:20:25 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow w; // Создаем главное окно
-    w.show(); // Показываем главное окно
+    w.show(); // Показываем главное окно hello
 
     return a.exec();
 }
